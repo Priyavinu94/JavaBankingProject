f0838b203d62653b9fd5a94ba1f2416f24a499f1
Priya <sreepdsy1994@gmail.com>
2022-02-17 02:19:48 -0500

    Update main class. Pending change pin/password functionality

--- src/bankingProject/BankingAppMain.java
+++ src/bankingProject/BankingAppMain.java
@@ -16,7 +16,10 @@ public class BankingAppMain {
 		System.out.println("Welcome to Banking Application\n");
 		System.out.println("Please select the service you want\n 1. ATM\n 2. Online Banking");
 		String choice = sc.next();
-		if (choice.equals("1")) {
+
+		switch (choice) {
+		/*********** ATM *****************/
+		case "1":
 			bank = new ATM();
 			while (attemptsLeft-- >= 0) {
 				System.out.println("Please provide your card number");
@@ -24,12 +27,12 @@ public class BankingAppMain {
 				System.out.println("Enter the 4 digit PIN");
 				String pinNumber = sc.next();
 				if (bank.isUserValid(cardNumber, pinNumber)) {
-				attemptsLeft = 3;
+					attemptsLeft = 3;
 					do {
 						System.out.println("Welcome " + bank.person.accountHolderName);
 						System.out.println("Please select the service you need");
-						System.out.println("1. Deposit\n2. Withdraw\n3 .Fund transfer"
-								+ "\n4. InvestmentCalculator\n5. CheckBalance");
+						System.out.println(
+								"1. Deposit\n2. Withdraw\n3 .Fund transfer" + "\n4. Investment\n5. CheckBalance");
 						String option = sc.next();
 						switch (option) {
 						case "1":
@@ -49,7 +52,7 @@ public class BankingAppMain {
 							bank.investment(0.0);
 							break;
 						case "5":
-							System.out.println("Your current balance is : $"+bank.getBalance());
+							System.out.println("Your current balance is : $" + bank.getBalance());
 							break;
 						default:
 							System.out.println("Please make a valid selection");
@@ -64,8 +67,64 @@ public class BankingAppMain {
 				System.out.println("Thank You! Have a great day!!");
 				break;
 			}
-		}
+			System.out.println("You already had 3 failed login attempts. Account is locked");
+			break;
 
+		/*********** Online Banking *****************/
+		case "2":
+			bank = new OnlineBanking();
+			while (attemptsLeft-- >= 0) {
+				System.out.println("Enter Card Number");
+				String cardNumber = sc.next();
+				System.out.println("Enter Online Banking Password");
+				String password = sc.next();
+				if (bank.isUserValid(cardNumber, password)) {
+					attemptsLeft = 3;
+					do {
+						System.out.println("Welcome " + bank.person.accountHolderName);
+						System.out.println("Please select the service you need");
+						System.out.println("1. Deposit\n2. Withdraw\n3 .Fund transfer"
+								+ "\n4. InvestmentCalculator\n5. CheckBalance");
+						String option = sc.next();
+						switch (option) {
+						case "1":
+							bank.deposit(0.0);
+							break;
+						case "2":
+							bank.withdraw(0.0);
+							break;
+						case "3":
+							System.out.println("Enter the amount you need to transfer");
+							double amountToTransfer = sc.nextDouble();
+							bank.fundTransfer(amountToTransfer);
+							break;
+						case "4":
+							System.out.println("Enter the amount to be deposited");
+							double amountToInvest = sc.nextDouble();
+							bank.investment(amountToInvest);
+							break;
+						case "5":
+							System.out.println("Your current balance is : $" + bank.getBalance());
+							break;
+						default:
+							System.out.println("Please make a valid selection");
+						}
+						System.out.println("Enter '1' to go back to Main Screen OR '0' to Exit");
+					} while (sc.next().equals("1"));
+				} else {
+					System.out.println("Invalid Card Number or Password. Please try again.");
+					attemptsLeft--;
+					continue;
+				}
+				System.out.println("Thank You! Have a great day!!");
+				break;
+			}
+			System.out.println("You already had 3 failed login attempts. Account is locked");
+			break;
+
+		default:
+			System.out.println("Please make a valid selection");
+		}
 	}
 
 }
